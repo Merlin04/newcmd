6aff9aed589f7abab09c6183d1776eac71b077dd
Merlin04 <Merlin04@users.noreply.github.com>
2015-09-19 13:26:24 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ if [ "$1" = "install"]; then
 sudo cp newcmd /bin/newcmd
 sudo chmod +x /bin/newcmd
 echo "Done"
-elif [ "$1" = "remove"]; then
+else if [ "$1" = "remove"]; then
 sudo rm /bin/newcmd
 echo "Done"
 else
